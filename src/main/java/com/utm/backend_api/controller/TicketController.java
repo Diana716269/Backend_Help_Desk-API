7c6eb5cbd0e0d209599b5481e30298faf5885cb2
DIANA CAROLINA ACUÑA ACUÑA <149345993+Diana716269@users.noreply.github.com>
2026-08-08 16:22:48 -0500

    Update TicketController.java

--- src/main/java/com/utm/backend_api/controller/TicketController.java
+++ src/main/java/com/utm/backend_api/controller/TicketController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tickets")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 
 public class TicketController {
     @Autowired
